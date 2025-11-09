e64f0a589a80468a9133c20ae1b03d399f99d0d6
Sebastian Seitz <bassistsagainstracists@gmx.net>
2025-11-09 09:38:20 +0100

    add response stream

---
+++ ollama/stream.go
@@ -0,0 +1,60 @@
+package ollama
+
+import (
+	"encoding/json/jsontext"
+	"encoding/json/v2"
+	"errors"
+	"io"
+	"iter"
+)
+
+type ResponseStream struct {
+	body    io.ReadCloser
+	decoder *jsontext.Decoder
+	err     error
+}
+
+func NewResponseStream(in io.ReadCloser) *ResponseStream {
+	return &ResponseStream{
+		body:    in,
+		decoder: jsontext.NewDecoder(in),
+	}
+}
+
+func (r *ResponseStream) Err() error {
+	if errors.Is(r.err, io.EOF) {
+		return nil
+	}
+
+	return r.err
+}
+
+func (r *ResponseStream) Close() error {
+	return r.body.Close()
+}
+
+func (r *ResponseStream) Recv() (ChatResponse, error) {
+	var part ChatResponse
+
+	if r.err != nil {
+		return part, r.err
+	}
+
+	err := json.UnmarshalDecode(r.decoder, &part)
+	if err != nil {
+		r.err = err
+	}
+
+	return part, err
+}
+
+func (r *ResponseStream) Iter() iter.Seq[ChatResponse] {
+	return func(yield func(ChatResponse) bool) {
+		for {
+			part, err := r.Recv()
+			if err != nil || !yield(part) {
+				return
+			}
+		}
+	}
+}
