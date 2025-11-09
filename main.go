74bfd0dcdee337c88b6985b29c9dd196157c91be
Sebastian Seitz <bassistsagainstracists@gmx.net>
2025-11-09 11:41:10 +0100

    add cli flags

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json/v2"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"m3g4p0p/agents/ollama"
 
@@ -16,13 +19,31 @@ func init() {
 }
 
 func main() {
-	client := ollama.Client{BaseURL: "http://localhost:11434", Model: "qwen3:1.7b"}
+	var options struct {
+		model string
+		raw   bool
+	}
+
+	flag.StringVar(&options.model, "model", "qwen3:1.7b", "")
+	flag.BoolVar(&options.raw, "raw", false, "")
+	flag.Parse()
+
+	client := ollama.Client{
+		BaseURL: "http://localhost:11434",
+		Model:   options.model,
+	}
+
 	stream, err := client.Chat([]ollama.ChatMessage{{Role: "user", Content: "Say hello world!"}})
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for part := range stream.Iter() {
-		fmt.Print(part.Message.Content)
+		if options.raw {
+			json.MarshalWrite(os.Stdout, part)
+			fmt.Fprintln(os.Stdout)
+		} else {
+			fmt.Print(part.Message.Content)
+		}
 	}
 }
