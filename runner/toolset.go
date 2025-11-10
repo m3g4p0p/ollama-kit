2643d1a373d51875122cdfd76abbb939bf55e845
Sebastian Seitz <bassistsagainstracists@gmx.net>
2025-11-10 11:51:07 +0100

    add toolset

---
+++ runner/toolset.go
@@ -0,0 +1,40 @@
+package runner
+
+import (
+	"encoding/json"
+
+	"m3g4p0p/agents/ollama"
+
+	"github.com/google/jsonschema-go/jsonschema"
+)
+
+type toolset struct {
+	tools    []ollama.Tool
+	handlers map[string]func(any) string
+}
+
+type toolOption func(t *toolset)
+
+func WithTool[T any](name, description string, handler func(T) string) toolOption {
+	schema, err := jsonschema.For[T](&jsonschema.ForOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	return func(t *toolset) {
+		t.tools = append(t.tools, ollama.Tool{
+			Type: "function",
+			Function: ollama.ToolFunction{
+				Name:        name,
+				Description: description,
+				Parameters:  schema,
+			},
+		})
+
+		t.handlers[name] = func(a any) string {
+			var args T
+			json.Unmarshal(a.([]byte), &args)
+			return handler(args)
+		}
+	}
+}
