994e8c86ed556863a5b28a2cb2c5745a5b601d25
Sebastian Seitz <bassistsagainstracists@gmx.net>
2025-11-22 16:02:25 +0100

    add deleter factory

--- agentlib/history/processor.go
+++ agentlib/history/processor.go
@@ -8,30 +8,6 @@ import (
 
 type Processor func(history []ollama.ChatMessage) []ollama.ChatMessage
 
-func RemoveRole(role string) Processor {
-	return func(history []ollama.ChatMessage) []ollama.ChatMessage {
-		return slices.DeleteFunc(history, func(msg ollama.ChatMessage) bool {
-			return msg.Role == role
-		})
-	}
-}
-
-func RemoveRoles(roles ...string) Processor {
-	return func(history []ollama.ChatMessage) []ollama.ChatMessage {
-		return slices.DeleteFunc(history, func(msg ollama.ChatMessage) bool {
-			return slices.Contains(roles, msg.Role)
-		})
-	}
-}
-
-func RemoveToolCalls() Processor {
-	return func(history []ollama.ChatMessage) []ollama.ChatMessage {
-		return slices.DeleteFunc(history, func(msg ollama.ChatMessage) bool {
-			return len(msg.ToolCalls) > 0
-		})
-	}
-}
-
 func Combine(processors ...Processor) Processor {
 	return func(history []ollama.ChatMessage) []ollama.ChatMessage {
 		for _, p := range processors {
@@ -40,4 +16,28 @@ func Combine(processors ...Processor) Processor {
 
 		return history
 	}
+}
+
+func Deleter(del func(msg ollama.ChatMessage) bool) Processor {
+	return func(history []ollama.ChatMessage) []ollama.ChatMessage {
+		return slices.DeleteFunc(history, del)
+	}
+}
+
+func DeleteRole(role string) Processor {
+	return Deleter(func(msg ollama.ChatMessage) bool {
+		return msg.Role == role
+	})
+}
+
+func DeleteRoles(roles ...string) Processor {
+	return Deleter(func(msg ollama.ChatMessage) bool {
+		return slices.Contains(roles, msg.Role)
+	})
+}
+
+func DeleteToolCalls() Processor {
+	return Deleter(func(msg ollama.ChatMessage) bool {
+		return len(msg.ToolCalls) > 0
+	})
 }
